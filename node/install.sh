a796486886e48bbeadf63b905143737383dfe791
Josh Eaton <josh@josheaton.org>
2016-11-14 14:45:28 -0500

    Add npm-check-updates to node install

--- node/install.sh
+++ node/install.sh
@@ -12,7 +12,7 @@ nvm install node
 nvm use node
 nvm alias default node
 
-npm install -g aglio babel-cli bower coffee-script dredd ejs-compile eslint grunt-cli gulp jshint json mocha phplint trash-cli
+npm install -g aglio babel-cli bower coffee-script dredd ejs-compile eslint grunt-cli gulp jshint json mocha npm-check-updates phplint trash-cli
 
 echo ''
 echo 'You may want to check your ~/.zshrc to remove any lines added by the installer.'
