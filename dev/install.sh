0bb7a479e8ae9b1fe7f3634bcbbf0902fc43cbab
Josh Eaton <josh@josheaton.org>
2016-11-15 22:00:38 -0500

    Add initial local dev installation

---
+++ dev/install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# #############################################################################
+# Install local development environment
+# #############################################################################
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir -p $HOME/Sites
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer \
+	laravel/lumen-installer \
+	laravel/valet psy/psysh \
+	apigen/apigen behat/behat
+
+# Install Laravel Valet
+$HOME/.composer/vendor/bin/valet install
+valet domain app
+
+# Install wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+wp --info
+
+# Install vvv dependencies
+
+# install vvv if not exists
+
+# Configure vvv
