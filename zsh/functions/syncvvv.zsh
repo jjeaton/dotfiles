7d5cf3efa46c3e102fa2893ac7930a34dec2e0f3
Josh Eaton <josh@josheaton.org>
2016-11-23 12:20:51 -0500

    Update syncvvv to only sync nginx sites config

--- zsh/functions/syncvvv.zsh
+++ zsh/functions/syncvvv.zsh
@@ -4,11 +4,11 @@ function syncvvv() {
 
 	case "$type" in
 		backup )
-			echo 'Backing up vvv/config...'
-			cp -r ~/Sites/vvv/config ~/Temp
-			cp -r ~/Sites/vvv/config ~/Dropbox/Backups/vvv/
-			rm -rf ~/Sites/vvv/config
-			ln -s ~/Dropbox/Backups/vvv/config ~/Sites/vvv/config
+			echo 'Backing up vvv/config/nginx-config/sites...'
+			cp -r ~/Sites/vvv/config/nginx-config/sites ~/Temp/
+			cp -r ~/Sites/vvv/config/nginx-config/sites ~/Dropbox/Backups/vvv/config/nginx-config/
+			rm -rf ~/Sites/vvv/config/nginx-config/sites
+			ln -s ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
 			echo 'Backing up vvv/database...'
 			cp -r ~/Sites/vvv/database ~/Temp
 			cp -r ~/Sites/vvv/database ~/Dropbox/Backups/vvv/
@@ -17,16 +17,16 @@ function syncvvv() {
 			;;
 		restore )
 			echo 'Restoring vvv/config from Dropbox...'
-			rm -rf ~/Sites/vvv/config
-			ln -s ~/Dropbox/Backups/vvv/config ~/Sites/vvv/config
+			rm -rf ~/Sites/vvv/config/nginx-config/sites
+			ln -s ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
 			echo 'Restoring vvv/database from Dropbox...'
 			rm -rf ~/Sites/vvv/database
 			ln -s ~/Dropbox/Backups/vvv/database ~/Sites/vvv/database
 			;;
 		uninstall )
 			echo 'Unlinking vvv/config from Dropbox...'
-			rm -rf ~/Sites/vvv/config
-			cp -r ~/Dropbox/Backups/vvv/config ~/Sites/vvv/config
+			rm -rf ~/Sites/vvv/config/nginx-config/sites
+			cp -r ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
 			echo 'Unlinking vvv/database from Dropbox...'
 			rm -rf ~/Sites/vvv/database
 			cp -r ~/Dropbox/Backups/vvv/database ~/Sites/vvv/database
