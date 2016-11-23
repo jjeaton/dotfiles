77c7309caa64fe179bed733362de8474df920407
Josh Eaton <josh@josheaton.org>
2016-11-23 14:07:55 -0500

    Convert syncvvv into a copy script remove links

--- zsh/functions/syncvvv.zsh
+++ zsh/functions/syncvvv.zsh
@@ -4,32 +4,20 @@ function syncvvv() {
 
 	case "$type" in
 		backup )
-			echo 'Backing up vvv/config/nginx-config/sites...'
-			cp -r ~/Sites/vvv/config/nginx-config/sites ~/Temp/
+			echo 'Backing up vvv/config/nginx-config/sites to Dropbox...'
 			cp -r ~/Sites/vvv/config/nginx-config/sites ~/Dropbox/Backups/vvv/config/nginx-config/
-			rm -rf ~/Sites/vvv/config/nginx-config/sites
-			ln -s ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
+
 			echo 'Backing up vvv/database...'
-			cp -r ~/Sites/vvv/database ~/Temp
 			cp -r ~/Sites/vvv/database ~/Dropbox/Backups/vvv/
-			rm -rf ~/Sites/vvv/database
-			ln -s ~/Dropbox/Backups/vvv/database ~/Sites/vvv/database
 			;;
 		restore )
 			echo 'Restoring vvv/config from Dropbox...'
 			rm -rf ~/Sites/vvv/config/nginx-config/sites
-			ln -s ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
+			cp -r ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/
+
 			echo 'Restoring vvv/database from Dropbox...'
 			rm -rf ~/Sites/vvv/database
-			ln -s ~/Dropbox/Backups/vvv/database ~/Sites/vvv/database
-			;;
-		uninstall )
-			echo 'Unlinking vvv/config from Dropbox...'
-			rm -rf ~/Sites/vvv/config/nginx-config/sites
-			cp -r ~/Dropbox/Backups/vvv/config/nginx-config/sites ~/Sites/vvv/config/nginx-config/sites
-			echo 'Unlinking vvv/database from Dropbox...'
-			rm -rf ~/Sites/vvv/database
-			cp -r ~/Dropbox/Backups/vvv/database ~/Sites/vvv/database
+			cp -r ~/Dropbox/Backups/vvv/database ~/Sites/vvv/
 			;;
 		* )
 			;;
