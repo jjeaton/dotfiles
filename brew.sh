d5a347d791d38a89dc546b5fb60f86b28ebc46c6
Josh Eaton <josh@josheaton.org>
2019-02-05 12:59:42 -0800

    Brew updates

--- brew.sh
+++ brew.sh
@@ -29,13 +29,12 @@ brew update
 # Upgrade any already-installed formula.
 brew upgrade
 
-brew tap bradp/vv
 brew tap caskroom/cask
 brew tap caskroom/fonts
 brew tap homebrew/boneyard
 brew tap homebrew/core
 brew tap homebrew/dupes
-brew tap homebrew/php
+# brew tap homebrew/php
 brew tap homebrew/services
 brew tap homebrew/versions
 
@@ -58,14 +57,13 @@ brew install ack
 brew install autojump
 brew install awscli
 brew install bash-completion
-brew install dark-mode
 brew install doxygen
 brew install dnsmasq
 brew install dos2unix
 brew install ffmpeg
 brew install flow
 brew install git
-brew install heroku
+brew install heroku/brew/heroku
 brew install httpie
 brew install hub
 brew install imagemagick --with-webp
@@ -75,31 +73,29 @@ brew install lftp
 brew install libav
 brew install libyaml
 brew install lnav
-
 brew install lynx
 
 brew install mackup
-brew install macvim
 confirm "Install mariadb? [y/N]" && brew install mariadb
 confirm "Install mongodb? [y/N]" &&  brew install mongodb
 brew install multimarkdown
 
+brew install nginx
 
 brew install redis
 brew install openssl
 brew install --with-openssl curl
 # https://github.com/laravel/valet/issues/329#issuecomment-287031905
 # https://stackoverflow.com/questions/26461966/osx-10-10-curl-post-to-https-url-gives-sslread-error/26538127#26538127
-brew install --with-homebrew-curl php71
-brew install php71-redis
+brew install php
+#brew install php71-redis
 # See https://github.com/Homebrew/homebrew-php/issues/2544 if issues.
-brew install php71-intl
-brew install php71-xdebug
+#brew install php71-intl
+#brew install php71-xdebug
 brew install phpunit
-brew install xdebug-osx
 
 brew install python
-brew install python3
+brew install python@2
 brew install ruby
 
 brew install s3cmd
@@ -109,11 +105,11 @@ brew install testssl
 brew install tmux
 brew install tree
 
-brew install vv
 brew install wakeonlan
 brew install watchman
 brew install wget
 brew install xz
+brew install yarn
 brew install youtube-dl
 brew install z
 brew install zsh
@@ -123,59 +119,50 @@ brew install zsh-syntax-highlighting
 # Casks
 brew cask install adium
 brew cask install aerial
-brew cask install alfred
+# brew cask install alfred
 brew cask install appcleaner
 brew cask install atom
 brew cask install bartender
 brew cask install bettertouchtool
+brew cask install betterzip
 brew cask install caffeine
 brew cask install cheatsheet
-brew cask install clarify
-brew cask install cloudup
-brew cask install codekit
-brew cask install crashplan
+# brew cask install cloudup
+# brew cask install codekit
+# brew cask install crashplan
 brew cask install dash
 brew cask install docker
 brew cask install dropbox
 brew cask install fantastical
 brew cask install fastscripts
-brew cask install firefox
+# brew cask install firefox
 brew cask install flux
-brew cask install fluid
-brew cask install go2shell
+# brew cask install fluid
+# brew cask install go2shell
 brew cask install google-chrome
 brew cask install hazel
-brew cask install iterm2
+# brew cask install iterm2
 brew cask install imageoptim
 brew cask install jumpcut
 brew cask install keyboard-maestro
-brew cask install lastpass
-brew cask install logitech-control-center
 brew cask install macgdbp
 brew cask install marked
 # brew cask install microsoft-office
 brew cask install nvalt
 brew cask install omnidisksweeper
-# brew cask install path-finder
 brew cask install phpstorm
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
-brew cask install quickbooks
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
 brew cask install recordit
 brew cask install rescuetime
 brew cask install sequel-pro
 brew cask install skim
 brew cask install skype
 brew cask install sketch
-brew cask install slack
-brew cask install sqlitebrowser
-brew cask install soulver
-brew cask install sourcetree
 brew cask install spotify
 brew cask install sublime-text
 brew cask install superduper
-brew cask install textexpander # Installs the old non-Sass version.
+# brew cask install textexpander # Installs the old non-Sass version.
 brew cask install transmit
-brew cask install tunnelbear
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install vagrant-manager
